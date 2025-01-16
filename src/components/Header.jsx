041327c684d0eb652a77c8d0f45afbf67d7b6c22
billn <billnyetherussianspy679@gmail.com>
2025-01-16 19:34:50 +0300

    sc

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header  ({onDarkModeClick, isDarkMode}) {
+    return (
+        <div>
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+        </div>
+    )
+}
